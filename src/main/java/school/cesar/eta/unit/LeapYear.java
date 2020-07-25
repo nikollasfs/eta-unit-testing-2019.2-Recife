fd29233258bd1966916d2771319ac60f96128b4a
Nikollas <nikollasfs22@gmail.com>
2020-07-25 18:02:51 -0300

    Add leap year

---
+++ src/main/java/school/cesar/eta/unit/LeapYear.java
@@ -0,0 +1,20 @@
+package school.cesar.eta.unit;
+
+public class LeapYear {
+    public static boolean isLeapYear;
+
+    public static boolean isLeapYear(int year) {
+
+        if (year % 400 == 0){
+            return true;
+        }
+
+        if (year % 100 == 0){
+            return false;
+        }
+
+        return year % 4 == 0;
+    }
+
+
+}
